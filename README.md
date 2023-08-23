1280e8578868a8afb0dc2b1f73b72a281610f1ca
Alexander Zinin <spore_09@mail.ru>
2023-08-23 21:50:50 +0400

    add base readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# Test
+
+## First run
+
+```bash
+$ npm install && npm run build && npm run db:init
+```
+
+## Run app.js
+
+```bash
+$ npm run app:start
+```
+
+## Run incremental sync
+
+```bash
+$ npm run sync:incr
+```
+
+## Run full sync
+
+```bash
+$ npm run sync:full
+```
--- src/sync/index.ts
+++ src/sync/index.ts
@@ -7,7 +7,7 @@ import { getLogger } from '../utils/logger';
 const mainLogger = getLogger('main');
 
 async function main() {
-  const runFullSync = process.argv.includes('--full-sync');
+  const runFullSync = process.argv.includes('--full-reindex');
   if (runFullSync) {
     mainLogger.info('Running full sync');
     await fullSync(process.env.MONGO_URL!);
