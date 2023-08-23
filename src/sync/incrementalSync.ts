b505306e95c5169fad60f99d74fd756987f42b39
Alexander Zinin <spore_09@mail.ru>
2023-08-24 01:02:36 +0400

    improve imcremental sync

--- src/sync/incrementalSync.ts
+++ src/sync/incrementalSync.ts
@@ -1,4 +1,7 @@
 import { Collection, MongoClient } from 'mongodb';
+import Queue from 'queue';
+import { debounce } from 'lodash';
+import { once } from 'node:events';
 import { ICustomerMeta, ICustomer } from '../types';
 import { batchRunner } from '../utils/batchRunner';
 import { sleep } from '../utils/sleep';
@@ -7,65 +10,92 @@ import { getLogger } from '../utils/logger';
 
 const logger = getLogger('incrementalSync');
 
-async function updateCustomers(
-  syncMeta: ICustomerMeta[],
+async function updateCustomer(
+  meta: ICustomerMeta,
   customersAnonCollection: Collection<ICustomer>,
   customersMetaCollection: Collection<ICustomerMeta>,
 ) {
-  let syncedCount = 0;
-  for (const meta of syncMeta) {
-    const actualMeta = await customersMetaCollection.findOneAndUpdate(
+  const metaUpdateResult = await customersMetaCollection.findOneAndUpdate(
+    {
+      isSynced: false,
+      customerId: meta.customerId,
+      version: meta.version,
+    },
+    {
+      $set: {
+        isSynced: true,
+        updatedAt: new Date(),
+      },
+    },
+  );
+
+  const isUpdated = !!metaUpdateResult.value;
+  if (!isUpdated) {
+    return;
+  }
+
+  try {
+    await customersAnonCollection.updateOne(
       {
-        isSynced: false,
-        customerId: meta.customerId,
-        version: meta.version,
+        _id: meta.customerId,
+      },
+      {
+        $set: anonymizeCustomer(meta.customerObject),
+      },
+      {
+        upsert: true,
+      },
+    );
+  } catch (err) {
+    logger.error('failed to update customer', meta.customerId, err);
+    await customersMetaCollection.updateOne(
+      {
+        _id: meta._id,
+        isSynced: true,
       },
       {
         $set: {
-          isSynced: true,
+          isSynced: false,
           updatedAt: new Date(),
         },
       },
     );
-
-    const isUpdated = !!actualMeta.value;
-    if (isUpdated) {
-      try {
-        await customersAnonCollection.updateOne(
-          {
-            _id: meta.customerId,
-          },
-          {
-            $set: anonymizeCustomer(meta.customerObject),
-          },
-          {
-            upsert: true,
-          },
-        );
-
-        syncedCount++;
-      } catch (err) {
-        logger.error('failed to update customer', meta.customerId, err);
-        await customersMetaCollection.updateOne(
-          {
-            _id: meta._id,
-            isSynced: true,
-          },
-          {
-            $set: {
-              isSynced: false,
-              updatedAt: new Date(),
-            },
-          },
-        );
-      }
-    }
   }
+}
 
-  logger.info('synced', syncedCount, 'customers');
+function getUpdatingQueue() {
+  const q = new Queue({
+    autostart: true,
+    concurrency: 4,
+  });
+  const queueStat = {
+    sync: 0,
+    errors: 0,
+  };
+
+  const writeStat = debounce(
+    () => {
+      logger.info('synced', queueStat, 'customers');
+    },
+    1000,
+    { maxWait: 5000 },
+  );
+
+  q.on('success', () => {
+    queueStat.sync++;
+    writeStat();
+  });
+  q.on('error', () => {
+    queueStat.errors++;
+    writeStat();
+  });
+
+  return q;
 }
 
 export async function incremenalSync(mongoUrl: string) {
+  const updatingQueue = getUpdatingQueue();
+
   const client = new MongoClient(mongoUrl, {
     readPreference: 'primary',
   });
@@ -77,13 +107,18 @@ export async function incremenalSync(mongoUrl: string) {
   );
   const customersMetaCollection =
     db.collection<ICustomerMeta>('customers_meta');
+
   const customerWriter = batchRunner<ICustomerMeta>(
     async (metas: ICustomerMeta[]) => {
-      await updateCustomers(
-        metas,
-        customersAnonCollection,
-        customersMetaCollection,
-      );
+      for (const meta of metas) {
+        updatingQueue.push(() =>
+          updateCustomer(
+            meta,
+            customersAnonCollection,
+            customersMetaCollection,
+          ),
+        );
+      }
     },
     {
       maxBatchSize: 1000,
@@ -104,7 +139,8 @@ export async function incremenalSync(mongoUrl: string) {
         },
       )
       .sort({ updatedAt: 1 })
-      .batchSize(1000);
+      .batchSize(1000)
+      .limit(5000);
 
     let hasData = false;
     while (await cursor.hasNext()) {
@@ -117,6 +153,11 @@ export async function incremenalSync(mongoUrl: string) {
 
     if (!hasData) {
       await sleep(100);
+    }
+
+    if (updatingQueue.length > 5000) {
+      logger.warn('queue is full, waiting for it to drain');
+      await once(updatingQueue, 'end');
     }
   }
 }
